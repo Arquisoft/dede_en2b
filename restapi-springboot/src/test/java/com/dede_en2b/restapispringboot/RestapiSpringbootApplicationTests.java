32634f1eef251b7263b9be46cc66d6fc64c0a429
Diego Martín Fernández <diegomartinfnz@gmail.com>
2022-02-28 16:22:39 +0100

    Creation of the rest api

---
+++ restapi-springboot/src/test/java/com/dede_en2b/restapispringboot/RestapiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dede_en2b.restapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
