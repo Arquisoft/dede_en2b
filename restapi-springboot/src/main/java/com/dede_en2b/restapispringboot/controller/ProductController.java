82f5c01591b2ba8524197982b3bbf5af2ff7b26c
Diego Martín Fernández <diegomartinfnz@gmail.com>
2022-02-28 17:29:09 +0100

    Creation of the rest api with basic functionalities

---
+++ restapi-springboot/src/main/java/com/dede_en2b/restapispringboot/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.dede_en2b.restapispringboot.controller;
+
+
+import com.dede_en2b.restapispringboot.model.Product;
+import com.dede_en2b.restapispringboot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/product/add")
+    public String addProduct(@RequestBody Product product){
+        productService.saveProduct(product);
+        return "New Product is added";
+    }
+
+    @GetMapping("/product/list")
+    public List<Product>getList() {
+        return productService.getAllProducts();
+
+    }
+
+
+}
---
+++ restapi-springboot/src/main/java/com/dede_en2b/restapispringboot/service/impl/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.dede_en2b.restapispringboot.service.impl;
+
+import com.dede_en2b.restapispringboot.model.Product;
+import com.dede_en2b.restapispringboot.repository.ProductRepository;
+import com.dede_en2b.restapispringboot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostConstruct
+    public void init(){
+        saveProduct(new Product());
+        saveProduct(new Product());
+        saveProduct(new Product());
+        saveProduct(new Product());
+    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<Product>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
+}
