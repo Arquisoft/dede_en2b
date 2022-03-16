5aef2c35a591a669c49b2dee06fbe493a45a30bc
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-03-16 21:41:37 +0100

    Made some advancements

--- webapp/src/components/Order/Order.tsx
+++ webapp/src/components/Order/Order.tsx
@@ -3,6 +3,11 @@ import React, {useContext, useState} from "react";
 import CartItemCheckout from "./CartItemCheckout";
 import Divider from '@mui/material/Divider';
 import {calculateTotal} from "../../helper/calculateCartTotal";
+import {Card, CardMedia} from "@mui/material";
+import "./Order.css";
+import Box from "@mui/material/Box";
+import logo from '../../img/shippingicon.jpg';
+import logoReceipt from '../../img/receipticon.png';
 
 export default function Order() {
 
@@ -23,12 +28,66 @@ export default function Order() {
             </div>
 
         <div className="shippingCost">
-            Shipping cost: {shippingCost}€
+            <Card variant="outlined" sx= {{display:'flex', maxWidth:950}}>
+
+                <CardMedia
+                    component="img"
+                    sx={{ width:150, height: 50 }}
+                    image={logo}
+                    alt="shippingCost"
+                />
+
+                <Box sx={{display:'flex', flex: 1}}>
+                    <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', flex: 3.5}}>
+                        <div className="item-detail">
+                            <div className="detail-info">
+                                <div className="item-name">
+                                    Shipping cost
+                                </div>
+                            </div>
+                        </div>
+                    </Box>
+
+                    <Divider orientation="vertical" flexItem />
+
+                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', pl: 1, pb: 1, flex : 1}}>
+                        <span>{shippingCost}€</span>
+                    </Box>
+
+                </Box>
+            </Card>
         </div>
 
         <div className="subTotal">
-            Subtotal: {subTotal}€
+            <Card variant="outlined" sx= {{display:'flex', maxWidth:950}}>
 
+                <CardMedia
+                    component="img"
+                    sx={{ width:150, height: 50 }}
+                    image={logoReceipt}
+                    alt="subTotal"
+                />
+
+                <Box sx={{display:'flex', flex: 1}}>
+                    <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', flex: 3.5}}>
+                        <div className="item-detail">
+                            <div className="detail-info">
+                                <div className="item-name">
+                                    Subtotal
+                                </div>
+                            </div>
+                        </div>
+                    </Box>
+
+                    <Divider orientation="vertical" flexItem />
+
+                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', pl: 1, pb: 1, flex : 1}}>
+                        <span>{subTotal}€</span>
+                    </Box>
+
+                </Box>
+            </Card>
         </div>
+
         </div>;
 }
