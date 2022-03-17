a32f74feaa11bfd91acd60d3c715e9702763655f
Diego Martín Fernández <diegomartinfnz@gmail.com>
2022-03-17 18:06:36 +0100

    Sidebar completed

--- webapp/src/components/Sidebar/Sidebar.tsx
+++ webapp/src/components/Sidebar/Sidebar.tsx
@@ -10,13 +10,12 @@ function Sidebar() {
                     return <li
                         key={key} 
                         className ="row" 
+                        
                         id={window.location.pathname == val.link ? "active" : ""}
                         onClick={() => (window.location.pathname = val.link)}>
                         {" "}
                         <div id="icon">{val.icon}</div> {" "}
-                        <div id="title">
-                            {val.title}
-                        </div>
+                        
                     </li>
                 }
                 )}
