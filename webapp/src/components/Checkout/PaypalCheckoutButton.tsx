7a68c6bdf3695d7abb4c41171e4c786c253be745
CarolinaUniovi <78871007+CarolinaUniovi@users.noreply.github.com>
2022-03-21 05:29:38 +0100

    improvement of the paypalChechoutButton

--- webapp/src/components/Checkout/PaypalCheckoutButton.tsx
+++ webapp/src/components/Checkout/PaypalCheckoutButton.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import paypal from 'paypal-checkout';
+import * as paypal from 'paypal-checkout';
+import order from '../Order/Order';
+import cartTotal from '../../helper/calculateCartTotal';
 
-const PaypalCheckoutButton = (order) => {
+const PaypalCheckoutButton = () => {
     const paypalConf = {
+        //configure the environment
         currency: 'EUR',
         env: 'sandbox',
         client: {
@@ -21,26 +24,21 @@ const PaypalCheckoutButton = (order) => {
 
     const PaypalButton = paypal.Button.driver('react', {React, ReactDom}); //returning
 
+    //set up a payment
     const payment = (data, actions) => {
         const payment = {
-            transactions: [
-                {
-                    amount: {
-                        total: order.total,
-                        currency: paypalConf.currency,
-                    },
-                    descripcion: 'Pagar',
-                    custom: order.custom || '',
-                    item_list: {
-                        items: order.items
-                    }
-                }
-            ]
+            transactions: [{
+                amount: {
+                    total: cartTotal.calculateTotal,
+                    currency: paypalConf.currency,
+                },
+            }]
         };
         return actions.payment.create({payment});
     };
 
-    const onAuthorize = (data, actions) =>{
+
+    const onAuthorize = (data, actions) => {
         return actions.payment.execute()
             .then(response =>{
                 console.log(response);
@@ -68,7 +66,7 @@ const PaypalCheckoutButton = (order) => {
             payment = {(data, actions) => payment(data, actions)}
             onAuthorize = {(data, actions) => onAuthorize(data, actions)}
             onCancel = {(data, actions) => onCancel(data, actions)}
-            onError = {(error) => onAuthorize(error)}
+            onError = {(error) => onError(error)}
             style = {paypalConf.style}
             commit
         />
