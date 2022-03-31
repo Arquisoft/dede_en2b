cc87420658975c92112179016aac767379dbd857
Diego <diegomartinfnz@gmail.com>
2022-03-31 18:30:23 +0200

    deploying prep

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,22 +1,21 @@
-import {OrderType, ProductType} from '../shared/shareddtypes';
+import { OrderType, ProductType } from "../shared/shareddtypes";
 
-
+const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000";
 const PRODUCT_LIST_API = "http://localhost:5000/product/list";
 const ORDER_LIST_API = "http://localhost:5000/order/listByUser";
 const ORDER_ID_API = "http://localhost:5000/order/";
 
-
-export async function getProductCatalog():Promise<ProductType[]>{
+export async function getProductCatalog(): Promise<ProductType[]> {
     let response = await fetch(PRODUCT_LIST_API);
-    return response.json()
+    return response.json();
 }
 
-export async function getOrdersForUser(webId:string):Promise<OrderType[]> {
-    let response = await fetch(ORDER_LIST_API + "/" + webId)
-    return response.json()
+export async function getOrdersForUser(webId: string): Promise<OrderType[]> {
+    let response = await fetch(ORDER_LIST_API + "/" + webId);
+    return response.json();
 }
 
-export async function getOrderById(orderId:string):Promise<OrderType> {
+export async function getOrderById(orderId: string): Promise<OrderType> {
     let response = await fetch(ORDER_ID_API + orderId);
     return response.json();
 }
