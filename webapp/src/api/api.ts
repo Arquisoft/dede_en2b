4d64aadc234fd10c3964a552571c3f73efa4a08d
Diego <diegomartinfnz@gmail.com>
2022-04-02 11:48:12 +0200

    Api endpoint configuration

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,9 +1,9 @@
 import { OrderType, ProductType } from "../shared/shareddtypes";
 
 const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000";
-const PRODUCT_LIST_API = "http://localhost:5000/product/list";
-const ORDER_LIST_API = "http://localhost:5000/order/listByUser";
-const ORDER_ID_API = "http://localhost:5000/order/";
+const PRODUCT_LIST_API = apiEndPoint + "/product/list";
+const ORDER_LIST_API = apiEndPoint + "/order/listByUser";
+const ORDER_ID_API = apiEndPoint + "/order/";
 
 export async function getProductCatalog(): Promise<ProductType[]> {
     let response = await fetch(PRODUCT_LIST_API);
