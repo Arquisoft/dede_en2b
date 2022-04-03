1a3f0842747333827132327b094612c184c73983
Diego <diegomartinfnz@gmail.com>
2022-04-03 18:52:56 +0200

    configuration

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -1,6 +1,8 @@
 import { OrderType, ProductType } from "../shared/shareddtypes";
 
-const apiEndPoint = process.env.REACT_APP_API_URI || "http://localhost:5000";
+const apiEndPoint =
+    //process.env.REACT_APP_API_URI
+    "http://54.234.146.174:5000" || "http://localhost:5000";
 const PRODUCT_LIST_API = apiEndPoint + "/product/list";
 const ORDER_LIST_API = apiEndPoint + "/order/listByUser";
 const ORDER_ID_API = apiEndPoint + "/order/";
