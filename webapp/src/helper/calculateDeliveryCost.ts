cf199022af872ac06ad6c53a288c101407ee8745
uo276188 <uo276188@uniovi.es>
2022-04-03 19:51:47 +0200

    problems delivery cost

--- webapp/src/helper/calculateDeliveryCost.ts
+++ webapp/src/helper/calculateDeliveryCost.ts
@@ -2,10 +2,36 @@ import {useSession} from "@inrupt/solid-ui-react";
 import {getSolidDataset, getThing, getUrl, Thing, getStringNoLocale} from "@inrupt/solid-client";
 import {VCARD} from "@inrupt/vocab-common-rdf";
 
+async function GetPostalCode() : Promise<String>  {
+    const {session} = useSession();
+    let webId = session.info.webId as string;
+    let profileDocumentURI = webId.split("#")[0];
+    let dataSet = await getSolidDataset(profileDocumentURI);
+    let profile = getThing(dataSet, webId) as Thing;
+    let urlAdress = getUrl(profile, VCARD.hasAddress) as string;
+    let addressProfile = await getThing(dataSet, urlAdress);
+    let postalCode = getStringNoLocale(addressProfile as Thing, VCARD.postal_code) as string;
+    return postalCode;
+}
 
+const getCode = async ()  => {
+    let code = await GetPostalCode();
+    return code;
+}
+
+const convert = () : number => {
+    let ne = getCode();
+    let result : number = 0;
+    ne.then(value => {
+        let s = value;
+        let n : number = +s;
+        result = n;
+    })
+    return result;
+}
 
 export const calculateDeliveryCost = (): number => {
-    let code = getCode() as number
+    let code: number = convert();
     if (code >= 1000 && code <=50840 ){ //spain
         if (code >=7000 && code <= 7860){ //baleares
             return 7.0
@@ -24,24 +50,3 @@ export const calculateDeliveryCost = (): number => {
     return 15.5 //not spain
 }
 
-const getCode = () : number => {
-    let promise = Promise.resolve(GetPostalCode());
-    promise.then(value => {
-        let postalCode:number = parseInt(value, 100);
-
-        return postalCode
-    })
-    return 0
-}
-
-async function GetPostalCode()  {
-    const {session} = useSession();
-    let webId = session.info.webId as string;
-    let profileDocumentURI = webId.split("#")[0];
-    let dataSet = await getSolidDataset(profileDocumentURI);
-    let profile = getThing(dataSet, webId) as Thing;
-    let urlAdress = getUrl(profile, VCARD.hasAddress) as string;
-    let addressProfile = await getThing(dataSet, urlAdress);
-    let postalCode = getStringNoLocale(addressProfile as Thing, VCARD.postal_code) as string;
-    return postalCode;
-}
