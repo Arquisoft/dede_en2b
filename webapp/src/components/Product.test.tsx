8f30356da5b75d2c5525b160d34473f5865412ea
Laura Pernía <78539552+laura-pb@users.noreply.github.com>
2022-04-04 00:16:02 +0200

    Fix code smell 3

--- webapp/src/components/Product.test.tsx
+++ webapp/src/components/Product.test.tsx
@@ -6,6 +6,6 @@ import {ProductType} from "../shared/shareddtypes";
 
 test('check that a product renders properly', async () => {
     const productExample:ProductType = {id:1, name:"Watermelon", category:"Fruit", price:1.12, image:"https://images.unsplash.com/photo-1629084092232-b7b3fa74cd4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" };
-    const {getByText} = render( <Product product={productExample} handleAddToCart={(product:ProductType) => 0 }/>);
+    const {getByText} = render( <Product product={productExample} handleAddToCart={(_product:ProductType) => 0 }/>);
     expect(getByText(productExample.name)).toBeInTheDocument();
 });
