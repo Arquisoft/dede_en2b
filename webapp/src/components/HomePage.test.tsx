747c8ac43bc9ce887e272213262a1c18983fffca
Laura Pernía <78539552+laura-pb@users.noreply.github.com>
2022-04-04 00:15:39 +0200

    Fix code smell 2

--- webapp/src/components/HomePage.test.tsx
+++ webapp/src/components/HomePage.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {fireEvent, render, screen} from '@testing-library/react';
 import App from '../App';
-import HomePage from './HomePage';
 
 test('homepage title renders', () => {
     render(<App />);
