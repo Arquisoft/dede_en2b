c8e6ac87bd717b7af1be1b19ecbe1eaefe5caf86
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-04-16 13:31:19 +0200

    Fixed the delivery cost calculation, before it only worked when debugging, also fixed the delivery cost calculation, and now it is added to the cost of the products

--- webapp/src/helper/calculateDeliveryCost.ts
+++ webapp/src/helper/calculateDeliveryCost.ts
@@ -1,31 +1,37 @@
 import {useSession} from "@inrupt/solid-ui-react";
 import {getSolidDataset, getThing, getUrl, Thing, getStringNoLocale} from "@inrupt/solid-client";
 import {VCARD} from "@inrupt/vocab-common-rdf";
-import {setValue, getValue} from "./deliveryAux";
-import {DeliveryHelper} from "../shared/shareddtypes";
 
-export const calculateDeliveryCost = (): number => {
-    GetPostalCode().then(r => console.log(r));
-    let code: number = getValue();
-    if (code >= 1000 && code <=50840 ){ //spain
-        if (code >=7000 && code <= 7860){ //baleares
-            return 7.0
+export const calculateDeliveryCost = (postalCode: number): number => {
+
+    let shippingCost = 0;
+
+    let code = postalCode;
+
+    //GetPostalCode().then(code => {
+        if (code >= 1000 && code <=50840 ){ //spain
+            if (code >=7000 && code <= 7860){ //baleares
+                shippingCost = 7.0
+            }
+            if ((code >=35000 && code <= 35640) || (code >=38000  && code <= 38911)){ //canarias
+                shippingCost = 8.5
+            }
+            if (code >=51000 && code <= 51001){ //ceuta
+                shippingCost = 6.0
+            }
+            if (code >= 52000 && code <= 52001){ //melilla
+                shippingCost = 6.0
+            }
+            shippingCost = 5.75;
+        } else {
+            shippingCost = 15.5; //not spain
         }
-        if ((code >=35000 && code <= 35640) || (code >=38000  && code <= 38911)){ //canarias
-            return 8.5
-        }
-        if (code >=51000 && code <= 51001){ //ceuta
-            return 6.0
-        }
-        if (code >= 52000 && code <= 52001){ //melilla
-            return 6.0
-        }
-        return 5.75
-    }
-    return 15.5 //not spain
+    //});
+
+    return shippingCost;
 }
 
-async function GetPostalCode(): Promise<string> {
+export async function GetPostalCode(): Promise<number> {
     const {session} = useSession();
     let webId = session.info.webId as string;
     let profileDocumentURI = webId.split("#")[0];
@@ -33,7 +39,47 @@ async function GetPostalCode(): Promise<string> {
     let profile = getThing(dataSet, webId) as Thing;
     let urlAddress = getUrl(profile, VCARD.hasAddress) as string;
     let addressProfile = await getThing(dataSet, urlAddress);
-    let postalCode = getStringNoLocale(addressProfile as Thing, VCARD.postal_code) as string; // el problema esta aqui
+    let postalCode = getStringNoLocale(addressProfile as Thing, VCARD.postal_code) as string;
 
-    return postalCode;
+    return parseInt(postalCode);
+}
+
+export async function GetAddress(): Promise<string> {
+    const {session} = useSession();
+    let webId = session.info.webId as string;
+    let profileDocumentURI = webId.split("#")[0];
+    let dataSet = await getSolidDataset(profileDocumentURI);
+    let profile = getThing(dataSet, webId) as Thing;
+    let urlAddress = getUrl(profile, VCARD.hasAddress) as string;
+    let addressProfile = await getThing(dataSet, urlAddress);
+    let address = getStringNoLocale(addressProfile as Thing, VCARD.street_address) as string;
+
+    return address;
+}
+
+export async function GetDeliveryCost(): Promise<number> {
+
+    let shippingCost = 0;
+
+    let code = await GetPostalCode();
+
+    if (code >= 1000 && code <=50840 ){ //spain
+        if (code >=7000 && code <= 7860){ //baleares
+            shippingCost = 7.0
+        }
+        if ((code >=35000 && code <= 35640) || (code >=38000  && code <= 38911)){ //canarias
+            shippingCost = 8.5
+        }
+        if (code >=51000 && code <= 51001){ //ceuta
+            shippingCost = 6.0
+        }
+        if (code >= 52000 && code <= 52001){ //melilla
+            shippingCost = 6.0
+        }
+        shippingCost = 5.75;
+    } else {
+        shippingCost = 15.5; //not spain
+    }
+
+    return shippingCost;
 }
