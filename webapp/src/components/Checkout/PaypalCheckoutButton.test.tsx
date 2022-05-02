478ccbbc60335174a73bf0554237e89f887e3dd7
Laura Pernía <laurapb.inf1617@gmail.com>
2022-05-02 20:48:11 +0200

    Tests for CompleteOrder, Cart footer and PaypalCheckoutButton

---
+++ webapp/src/components/Checkout/PaypalCheckoutButton.test.tsx
@@ -0,0 +1,14 @@
+import {render} from "@testing-library/react";
+import PaypalButton from "./PaypalCheckoutButton";
+import * as cost from "../../helper/calculateCartTotal"
+import * as shipping from "../../helper/calculateDeliveryCost"
+
+test("Check that for the paypal payment total order cost and shipping cost are called only once", async () => {
+    let costCall = jest.spyOn(cost, "calculateTotal").mockReturnValue(2);
+    let shippingCostCall = jest.spyOn(shipping, "GetDeliveryCost").mockReturnValue(Promise.resolve(1.3));
+
+    render(<PaypalButton payed={() => {}}/>)
+
+    expect(costCall).toHaveBeenCalledTimes(1);
+    expect(shippingCostCall).toHaveBeenCalledTimes(1);
+});
